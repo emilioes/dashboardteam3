ddebf5dc69e5ce052f100384754a212646b6ede5
emilioes <emil01155@gmail.com>
2023-03-24 09:48:49 -0600

    dashboard another try

--- script/dashBoard.py
+++ script/dashBoard.py
@@ -4,6 +4,26 @@ import plotly
 
 df = pd.read_csv("https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/owid-covid-data.csv")
 ###
+df = df[(df["location"] == "Mexico") & (df["people_fully_vaccinated"].notnull())]
+
+# Convert the date column to a datetime object
+df["date"] = pd.to_datetime(df["date"])
+
+st.title("COVID-19 Vaccination Data for Mexico")
+st.write("This app displays the number of people fully vaccinated against COVID-19 in Mexico over time.")
+    
+# Create a line chart using Altair
+chart = alt.Chart(df).mark_line().encode(
+   x=alt.X("date:T", title="Date"),
+   y=alt.Y("people_fully_vaccinated:Q", title="People Fully Vaccinated")
+).properties(
+     width=800,
+     height=500
+)
+
+
+df = pd.read_csv("https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/owid-covid-data.csv")
+###
 countries = ['United States', 'India', 'Mexico', 'France']
 country_cases = df.loc[df['location'].isin(countries)]
 country_cases = country_cases.groupby(['location', 'date'])['new_cases_smoothed_per_million'].sum().reset_index()
@@ -22,3 +42,5 @@ fig.update_layout(
 )
 
 st.plotly_chart(fig)
+
+
